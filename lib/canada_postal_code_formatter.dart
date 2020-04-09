6d43a97e9ac368707bb121ca34e1f6b502a1efb4
Simon Groom <sgroom@nextdimensioninc.com>
2020-04-09 17:28:51 -0400

    minor refactor and comments

--- lib/canada_postal_code_formatter.dart
+++ lib/canada_postal_code_formatter.dart
@@ -7,19 +7,26 @@ import 'package:flutter/services.dart';
 /// on each change of the input to ensure it matches the expected input at any
 /// given time.
 class CanadaPostalCodeFormatter extends TextInputFormatter {
-  static final firstString = "[ABCEGHJKLMNPRSTVXY]";
-  static final remainderString = "[ABCEGHJKLMNPRSTVWXYZ]";
+  // Forward Sortation Area (FSA) - Is the first 3 chars of the Canadian
+  // Postal Code. The first char cannot be W or Z, nor D, F, I, O, Q and U
+  // Local Delivery Unit (LDU) is the last 3 chars of Canadian Postal Code.
+  // LDU cannot contain D, F, I, O, Q and U
+  static const String validFsaFirstChar = "[ABCEGHJKLMNPRSTVXY]";
+  static const String validFsaAndLduChars = "[ABCEGHJKLMNPRSTVWXYZ]";
+  static const String validFsa = "$validFsaFirstChar[\\d]$validFsaAndLduChars";
+  static const String validLdu = "[\\d]$validFsaAndLduChars[\\d]";
+  static const String validPostalCode = "$validFsa[ ]$validLdu";
 
   /// [List] of [RegExp] representing the expected text input at all points
   /// of text input.
   static final List<RegExp> _regExp = [
-    RegExp(firstString),
-    RegExp("$firstString[\\d]"),
-    RegExp("$firstString[\\d]$remainderString"),
-    RegExp("$firstString[\\d]$remainderString[ ]"),
-    RegExp("$firstString[\\d]$remainderString[ ][\\d]"),
-    RegExp("$firstString[\\d]$remainderString[ ][\\d]$remainderString"),
-    RegExp("$firstString[\\d]$remainderString[ ][\\d]$remainderString[\\d]"),
+    RegExp(validFsaFirstChar),
+    RegExp("$validFsaFirstChar[\\d]"),
+    RegExp(validFsa),
+    RegExp("$validFsa[ ]"),
+    RegExp("$validFsa[ ][\\d]"),
+    RegExp("$validFsa[ ][\\d]$validFsaAndLduChars"),
+    RegExp(validPostalCode),
   ];
 
   /// Returns a [String]. Ensures the [newValue] matches the expected input,
@@ -28,6 +35,8 @@ class CanadaPostalCodeFormatter extends TextInputFormatter {
   String _validateNewText(
       TextEditingValue newValue, TextEditingValue oldValue, RegExp regExp) {
     String newText = newValue.text;
+    // check our regex - if we don't match expected, set the newText val to the
+    // oldValue text - rejecting the update.
     if (!regExp.hasMatch(newValue.text)) {
       newText = oldValue.text;
     }
@@ -45,13 +54,14 @@ class CanadaPostalCodeFormatter extends TextInputFormatter {
           _validateNewText(newValue, oldValue, _regExp[newTextLength - 1]);
     }
     if (newText.length == 3 && newText.length > oldValue.text.length) {
-      // add a space for the user, so they don't need to type it themselves
+      // if we have a valid FSA, and we aren't deleting characters, add a space
+      // for the user, so they don't need to type it themselves
       newText = newText + " ";
     }
+    // return our new value
     return TextEditingValue(
       text: newText,
       selection: TextSelection.collapsed(offset: newText.length),
     );
   }
 }
-
